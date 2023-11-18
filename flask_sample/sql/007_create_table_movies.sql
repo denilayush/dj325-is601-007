fa6d389e5e4eb3ae2ac0b60c45d1569476103274
DenilJain <denilayush@gmail.com>
2023-11-18 13:35:24 -0500

    added base sql file

---
+++ flask_sample/sql/007_create_table_movies.sql
@@ -0,0 +1,31 @@
+CREATE TABLE
+    IS601_Movies (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        -- title VARCHAR(64) NOT NULL,
+        -- open DECIMAL(10, 4) NOT NULL,
+        -- high DECIMAL(10, 4) NOT NULL,
+        -- low DECIMAL(10, 4) NOT NULL,
+        -- price DECIMAL(10, 4) NOT NULL,
+        -- volume INT NOT NULL,
+        -- latest_trading_day DATE NOT NULL,
+        -- previous_close DECIMAL(10, 4) NOT NULL,
+        -- -- reserved keywords must be wrapped in backticks
+        -- `change` DECIMAL(10, 4) NOT NULL,
+        -- change_percent DECIMAL(10, 4) NOT NULL,
+        -- created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        -- modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        -- UNIQUE KEY `symbol_latest_trading_day` (
+        --     `symbol`,
+        --     `latest_trading_day`
+        -- )
+    );
+
+    --     exact: 'false',
+    -- info: 'x',
+    -- year: '2',
+    -- page: 'x',
+    -- sort: '2',
+    -- endYear: '2',
+    -- startYear: '2',
+    -- titleType: '2',
+    -- limit: '2'
