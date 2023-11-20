f4cd2d0052f0bad27574e2530405e15ee39f5597
DenilJain <denilayush@gmail.com>
2023-11-20 14:23:07 -0500

    Created Table for data and a subclass For API calls

--- flask_sample/sql/007_create_table_movies.sql
+++ flask_sample/sql/007_create_table_movies.sql
@@ -1,31 +1,12 @@
 CREATE TABLE
     IS601_Movies (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        -- title VARCHAR(64) NOT NULL,
-        -- open DECIMAL(10, 4) NOT NULL,
-        -- high DECIMAL(10, 4) NOT NULL,
-        -- low DECIMAL(10, 4) NOT NULL,
-        -- price DECIMAL(10, 4) NOT NULL,
-        -- volume INT NOT NULL,
-        -- latest_trading_day DATE NOT NULL,
-        -- previous_close DECIMAL(10, 4) NOT NULL,
-        -- -- reserved keywords must be wrapped in backticks
-        -- `change` DECIMAL(10, 4) NOT NULL,
-        -- change_percent DECIMAL(10, 4) NOT NULL,
-        -- created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        -- modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        -- UNIQUE KEY `symbol_latest_trading_day` (
-        --     `symbol`,
-        --     `latest_trading_day`
-        -- )
+        apiId VARCHAR(16) NOT NULL,
+        title VARCHAR(128) NOT NULL,
+        titleType VARCHAR(16) NOT NULL,
+        releaseDate DATE NOT NULL,
+        imageUrl VARCHAR(256),
+        -- reserved keywords must be wrapped in backticks
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );
-
-    --     exact: 'false',
-    -- info: 'x',
-    -- year: '2',
-    -- page: 'x',
-    -- sort: '2',
-    -- endYear: '2',
-    -- startYear: '2',
-    -- titleType: '2',
-    -- limit: '2'
