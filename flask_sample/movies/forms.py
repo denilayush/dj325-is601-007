f04c14524aeb08bbc0cdcbe4240c3d6068d72af7
DenilJain <denilayush@gmail.com>
2023-11-21 15:54:16 -0500

    list page with filter form(remaining to append query other than title field)

--- flask_sample/movies/forms.py
+++ flask_sample/movies/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, validators, SubmitField
+from wtforms import SelectField, StringField, DecimalField, validators, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
 
 #dj325 20/11/23 
 class movieSearchForm(FlaskForm):
@@ -11,3 +12,12 @@ class movieForm(FlaskForm):
     releaseDate = StringField('Release Date (YYYY-MM-DD)', [validators.Regexp(r'^\d{4}-\d{2}-\d{2}$', message="Invalid date format")])
     imageUrl = StringField('Image URL', [validators.Length(min=1, max=256)])
     submit = SubmitField("Save")
+
+
+class movieFilterForm(FlaskForm):
+    title = StringField("Title", [Optional()])
+    titleType = StringField('Type', [Optional()])
+    releaseDate = StringField('Release Date (YYYY-MM-DD)', [Optional(),validators.Regexp(r'^\d{4}-\d{2}-\d{2}$', message="Invalid date format")])
+    sort = SelectField("Columns", [Optional()], choices=[])
+    order = SelectField("Order", [Optional()], choices=[("asc","+"), ("desc","-")])
+    submit = SubmitField("Search")
