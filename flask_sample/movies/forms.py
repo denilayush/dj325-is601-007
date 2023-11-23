5b5573cd81f0a929586ff9bbbdb08c5ea018f12e
DenilJain <denilayush@gmail.com>
2023-11-23 03:04:32 -0500

    added date filter

--- flask_sample/movies/forms.py
+++ flask_sample/movies/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, DecimalField, validators, SubmitField
+from wtforms import SelectField, StringField, DecimalField, validators, SubmitField, DateField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
+from datetime import datetime
 
 #dj325 20/11/23 
 class movieSearchForm(FlaskForm):
@@ -17,7 +18,8 @@ class movieForm(FlaskForm):
 class movieFilterForm(FlaskForm):
     title = StringField("Title", [Optional()])
     titleType = StringField('Type', [Optional()])
-    releaseDate = StringField('Release Date (YYYY-MM-DD)', [Optional(),validators.Regexp(r'^\d{4}-\d{2}-\d{2}$', message="Invalid date format")])
+    releaseDateStart = DateField('Release Date Start',format='%Y-%m-%d')
+    releaseDateEnd = DateField('Release Date End',format='%Y-%m-%d', default=datetime.today())
     sort = SelectField("Columns", [Optional()], choices=[])
     order = SelectField("Order", [Optional()], choices=[("asc","+"), ("desc","-")])
     submit = SubmitField("Search")
